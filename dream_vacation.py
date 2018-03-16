c78757816f8d36ddf8488bdc8d5a584be95f6ef0
Sobekhotep <vlad.zankevich@gmail.com>
2018-03-16 09:54:55 +0300

    Modified sandwiches.py

--- dream_vacation.py
+++ dream_vacation.py
@@ -5,17 +5,21 @@ def run():
     quiz_active = True
 
     while quiz_active:
+
         name = input("Hello, what's your name? ")
         vacation_place = input("Where do you want to spend the vacation? ")
         quiz_dict[name] = vacation_place
 
-        repeat = input("Wold you like to let another person respond? (yes/no) ")
-        if repeat == 'yes':
-            continue
-        elif repeat == 'no':
-            quiz_active = False
-        else:
-            print("Please, write 'yes' or 'no'.")
+        next_person_flag = True
+        while next_person_flag:
+            repeat = input("Wold you like to let another person respond? (yes/no) ")
+            if repeat == 'yes':
+                next_person_flag = False
+            elif repeat == 'no':
+                quiz_active = False
+                next_person_flag = False
+            else:
+                print("Please, write 'yes' or 'no'.")
 
     # The quiz is done, output results
     print("\n--- Quiz result ---")
